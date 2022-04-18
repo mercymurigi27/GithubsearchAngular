5500ec7eb33393f7f3b976efeb081ad0100ef0f8
mercymurigi27 <mercy.murigi@student.moringaschool.com>
2022-04-18 10:20:23 +0300

    create user and repository class

---
+++ src/app/github/github.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { User } from '../user';
+
+@Component({
+  selector: 'app-github',
+  templateUrl: './github.component.html',
+  styleUrls: ['./github.component.css']
+})
+export class GithubComponent implements OnInit {
+  user: User[]=[];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/user.ts
@@ -0,0 +1,6 @@
+export class User {
+    constructor(public name:string, public profile:any){
+        this.name=name;
+        this.profile=profile;
+    };
+}
