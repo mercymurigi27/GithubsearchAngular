5adbebf41bfe812bea0ed005de7bfc09f7a4d8f4
mercymurigi27 <mercy.murigi@student.moringaschool.com>
2022-04-19 22:20:45 +0300

    Add styles in directive

--- src/app/font.directive.ts
+++ src/app/font.directive.ts
@@ -7,7 +7,9 @@ export class FontDirective {
 
   constructor(private fontType:ElementRef) {
     this.fontType.nativeElement.style.fontSize="30px"
-    this.fontType.nativeElement.style.color="aqua"
+    this.fontType.nativeElement.style.color="brown"
+    this.fontType.nativeElement.style.fontFamily="geneva"
+
    }
 
 }
